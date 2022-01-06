619c91976b79026795259336fa3769a1416f90fa
Tobias Theel <tt@fino.digital>
2022-01-06 09:50:55 +0100

    update test in challenge 2

--- backend/challenge2/database/database_test.go
+++ backend/challenge2/database/database_test.go
@@ -4,12 +4,29 @@ import (
 	"testing"
 
 	"github.com/Clarilab/codechallenge/backend/challenge2/database"
+	"github.com/Clarilab/codechallenge/backend/challenge2/models"
 )
 
 func Test_Repository_GetByName(t *testing.T) {
 	repo := database.NewRepository()
 
 	result := repo.GetByName("finux")
+
+	if result == nil {
+		t.Error("Expected to find any result")
+	}
+
+	if len(result) != 2 {
+		t.Error("Expected to find two results")
+	}
+}
+
+func Test_Repository_Get(t *testing.T) {
+	repo := database.NewRepository()
+
+	result := repo.Get(models.SearchRequest{
+		CompanyName: "finux",
+	})
 
 	if result == nil {
 		t.Error("Expected to find any result")
